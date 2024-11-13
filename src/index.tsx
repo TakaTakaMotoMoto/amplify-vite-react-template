dd19e3e1d3a14d52442aeb8c93a03429ca7d2079
TakaTakaMotoMoto <takamoto.ide.sk@gmail.com>
2024-11-13 23:57:35 +0900

    add schema

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 // src/index.tsx
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // ReactDOM.createRoot は 'react-dom/client' からインポート
+import ReactDOM from 'react-dom/client';
+import { Authenticator } from '@aws-amplify/ui-react'; // Authenticator を追加
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports'; // Amplify 設定ファイル
 import App from './App';
+import '@aws-amplify/ui-react/styles.css'; // Authenticator 用のスタイルをインポート
 import './index.css';
 
 // Amplify 設定
-Amplify.configure(awsconfig);
+Amplify.configure(awsconfig); // aws-exports を使用
 
 // React 18 での新しいレンダリング方法
-const root = ReactDOM.createRoot(document.getElementById('root')!); // root 要素に対して root を作成
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Authenticator> {/* 認証のラップ */}
+      <App />
+    </Authenticator>
   </React.StrictMode>
 );
